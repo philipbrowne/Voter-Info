95117900093afbf68a4e394499324a1f1bd31d76
Philip Browne <pbrowne@gmail.com>
2021-09-05 07:12:55 -0400

    update models py with first proposed schema

--- models.py
+++ models.py
@@ -11,3 +11,20 @@ def connect_db(app):
     """Connect to database"""
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """User Model"""
+    __tablename__ = 'users'
+    username = db.Column(db.String(50), primary_key=True, unique=True)
+    password = db.Column(db.Text(255), nullable=False)
+    first_name = db.Column(db.Text(255), nullable=False)
+    last_name = db.Column(db.Text(255), nullable=False)
+    street_address = db.Column(db.Text(255), nullable=False)
+    apartment_number = db.Column(db.Text(255))
+    city = db.Column(db.Text(255), nullable=False)
+    state = db.Column(db.Text(50), nullable=False)
+    zip_code = db.Column(db.Integer(20), nullable=False)
+    date_of_birth = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    cell_phone_number = db.Column(db.String)
