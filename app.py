eddbb3e908b33ae6e6ac29c488065dfd7600d355
Philip Browne <pbrowne@gmail.com>
2021-09-05 06:56:40 -0400

    add boilerplate code to flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, session, flash, jsonify, abort, send_from_directory, url_for, json
+import json
+
+# from forms import NewUserForm, UserLoginForm
+# from models import connect_db, db, User, Score
+
+from sqlalchemy.exc import IntegrityError
+import os
+
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
+URI = os.environ.get('DATABASE_URL', 'postgresql:///spaceforce-db')
+if URI.startswith("postgres://"):
+    URI = URI.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = os.environ.get(
+    'SECRET_KEY', 'M8)\x92\xb6Gk\xeeR\xc7jr')
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+connect_db(app)
